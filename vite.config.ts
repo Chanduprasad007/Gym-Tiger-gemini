89980259edcf34a22c36df89fd73988b532cc7a1
Chanduprasad007 <31735042+Chanduprasad007@users.noreply.github.com>
2026-06-04 12:18:31 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Gym-Tiger-gemini/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
